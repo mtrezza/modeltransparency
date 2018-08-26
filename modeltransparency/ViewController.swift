54299f759a3fe041568d27507361e73473a2ae7a
Manuel Trezza <trezza.m@gmail.com>
2018-08-26 03:12:29 +0200

    initial

---
+++ modeltransparency/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  modeltransparency
+//
+//  Created by Manuel Trezza on 26.08.18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
